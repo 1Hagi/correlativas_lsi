9cfcfea55b4f22479a039b6e2b4f9e0ba9bf701f
Gomez Matias <140461358+GomezMatias123@users.noreply.github.com>
2026-03-02 12:17:11 -0300

    Solución local storage independiente para cada plan de estudio
    Se logro independizar los guardados locales en cada .html de cada plan de estudio de la carrera, se actualizo la función del botón de reset para que se borre cada progreso independiente sin afectar al otro.
    Se agrego imagen del colaborador con un pequeño hover para que cuando el cursor este encima del mismo se pueda visualizar mejor la imagen para no afectar drasticamente el CSS del proyecto (es opcional mantenerlo según parezca conveniente)

--- js/local_Storage.js
+++ js/local_Storage.js
@@ -1,3 +1,14 @@
+//Separo los localStorage de los dos archivos.html correspondiente a los planes
+function obtenerClavePlan(){
+    if(window.location.href.includes("plan_estudio_nuevo.html")){
+        return "estadoMaterias_nuevo";
+    }
+    if(window.location.href.includes("plan_estudio_anterior.html")){
+        return "estadoMaterias_anterior";
+    }
+    //se agrega un return a estadosMaterias por seguridad de que lo demas se realiza correctamente
+    return "estadoMaterias";
+}
 
 /*guarda los estados en que se marca las materias en el local storage*/
 function guardarEstadoMaterias(subjects){
@@ -6,16 +17,21 @@ function guardarEstadoMaterias(subjects){
         id: s._id,
         status: s._status
     }));
-    localStorage.setItem("estadoMaterias",JSON.stringify(data));
+    const clave = obtenerClavePlan();       
+    localStorage.setItem(clave,JSON.stringify(data));
 }
 /*Carga los estados guardados en el local storage desde el onload.js para que sea visible para el usuario cada vez que actualiza la web*/
 function cargarEstadoMaterias(subjects) {
-    const data = JSON.parse(localStorage.getItem("estadoMaterias"));
+    const clave = obtenerClavePlan();
+    const data = JSON.parse(localStorage.getItem(clave));
+
     if (!data) return;
 
     data.forEach(saved => {
         if (saved.id === undefined) return;
+
         const subject = subjects.find(s => s._id === saved.id);
+
         if (subject) {
             subject._status = saved.status;
             subject.update(saved.status);
